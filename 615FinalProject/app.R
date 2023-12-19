0fe7ddd43e582d54278bf6ae4b998fb80cd8e7fb
NtwWzh <ngtszwai@bu.edu>
2023-12-18 19:20:31 -0500

    1

--- 615FinalProject/app.R
+++ 615FinalProject/app.R
@@ -1,43 +1,199 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
 library(leaflet)
+library(DT) 
 library(sf)
 
-
-# Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Map of Singapore"),
+  titlePanel("Singapore Analysis"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      tabsetPanel(
+        id = "tabs",  # 添加id属性
+        tabPanel("Map", value = "map"),
+        tabPanel("Key Demographics", value = "demographics"),
+        tabPanel("Comparison", value = "comparison"),
+        tabPanel("SWOT Analysis", value = "swot"),
+        tabPanel("Reference", value = "references")
+      )
+    ),
     
-    leafletOutput("map"),
-    
-    dataTableOutput("KeyFacts"),
-    
-    textOutput("description")
+    mainPanel(
+      # 使用uiOutput可以在server端控制显示哪个输出
+      uiOutput("tabContent")
+    )
+  )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
 
-  singapore_data <- st_layers("gadm41_SGP.gpkg")
+server <- function(input, output, session) {
   
-  # 选择要读取的图层
-  # layer_name <- "YOUR_LAYER_NAME"
-  # singapore_data <- st_read("gadm41_SGP.gpkg", layer = layer_name)
-  outpout$summary_output <- renderPrint({summary(singapore_data)})
+  output$tabContent <- renderUI({
+    # 根据选择的标签页，返回不同的UI组件
+    switch(input$tabs,
+           map = {
+             list(
+               leafletOutput("map"),
+               dataTableOutput("keyFactsTable"),
+               textOutput("description")
+             )
+           },
+           demographics = {
+             list(
+               dataTableOutput("DemographicTable"),
+               textOutput("demographicetext")
+             )
+           },
+           comparison = {
+             list(
+               dataTableOutput("comparisontablesocial"),
+               textOutput("comparisontext"),
+               dataTableOutput("comparisontableenvironment")
+             )
+           },
+           swot = {
+             list(
+               textOutput("description")
+             )
+           },
+           references = {
+             list(
+               textOutput("reference")
+             )
+           }
+    )
+  })
   
-  output$plot_output <- renderPlot({plot(singapore_data)})
+  
+  singapore_data <- st_read("gadm41_SGP.gpkg")
+  
+  # 渲染地图
+  output$map <- renderLeaflet({
+    if (input$tabs == "map") {
+      leaflet() %>%
+        addProviderTiles("OpenStreetMap.Mapnik") %>%
+        addPolygons(data = singapore_data, fillOpacity = 0.5)
+    }
+  })
+  
+  # 渲染"Map"下的关键事实表格和描述
+  output$keyFactsTable <- renderDataTable({
+    key_facts_df <- data.frame(
+      "General Information" = c("Region", "Capital city", "UN membership date", "National currency", "Official language"),
+      "Key Facts" = c("South-eastern Asia", "Singapore", "21-Sep-65", "Singapore Dollar (SGD)", "English/Chinese/Malay/Tamil")
+    )
+    datatable(key_facts_df)
+  })
+  
+  output$description <- renderText({
+    "Singapore is an island state located in Southeastern Asia. In Malay, 
+  the country name is Singapura and it means Lion City. It is known for its modernity, 
+  high population density, and diverse culture. The country has a rich history and is a 
+  major financial and business hub in Southeastern Asia."
+  })
+  
+  
+  # 渲染"Key Demographics"下的人口统计表格
+  output$DemographicTable <- renderDataTable({
+    if (input$tabs == "demographics") {
+      demographic_df <- data.frame(
+        Information = c("Population (000, 2021)", "Pop. density (per km2, 2021)", "Capital city pop. (000, 2021)",
+                        "Surface area (km2)", "Sex ratio (m per 100 f)", "Exchange rate (per US$)",
+                        "GDP: Gross domestic product (million current US$)",
+                        "GDP growth rate (annual %, const. 2015 prices)", 
+                        "Economy: Agriculture (% of Gross Value Added)"
+                        , "Economy: Industry (% of Gross Value Added)"
+                        , "Economy: Services and other activity (% of GVA)"
+                        , "Employment in agriculture (% of employed)"
+                        , "Employment in industry (% of employed)"
+                        , "Employment in services (% employed)", 
+                        "Unemployment (% of labour force)",
+                        "Labour force participation rate (female/male pop. %) - Female / Male",
+                        "CPI: Consumer Price Index (2010=100)",
+                        "International trade: exports (million current US$)",
+                        "International trade: imports (million current US$)",
+                         "Balance of payments, current account (million US$)"),
+        Value = c("5,897", "8,423.8", "5,868.1","725","109.8", "1.3","372,074","0.7","-0.0","25.8",
+                  "74.2","0.7","15.2","84.1","4.4","61.8/78.2","114","362,928","329,032","59,786")
+      )
+      datatable(demographic_df,options = list(scrollY = '1080px', scrollCollapse = TRUE))
+    }
+  })
+  
+  output$demographicetext <- renderText({
+    if(input$tabs == "demographics"){
+      "In conclusion, Singapore's Gross Domestic Product (GDP) reached $372074 million, 
+      showing stable growth, especially with an average annual growth rate of 0.7% based 
+      on constant prices in 2015. Singapore's economy is mainly dominated by the service industry,
+      accounting for 74.2% of the gross domestic product. The service industry is the main sector
+      of employment in Singapore, accounting for the highest proportion of employment at 84.1%. 
+      The unemployment rate can be considered relatively low. However, the consumption level is
+      high."
+    }
+  })
+  
+  output$comparisontablesocial <- renderDataTable({
+    if(input$tabs == "comparison"){
+      comparison_df_social <- data.frame(
+        Information = c("Population growth ratei (average annual %)", 
+                        "Urban population (% of total population)", 
+                        "Urban population growth ratei (average annual %)",
+                        "Fertility rate, totali (live births per woman)", 
+                        "Life expectancy at birthi (females/males, years)", 
+                        "Population age distribution (0-14/60+ years old, %)", 
+                        "International migrant stock (000/% of total pop.)", 
+                        "Refugees and others of concern to UNHCR (000)", 
+                        "Infant mortality ratei (per 1 000 live births)", 
+                        "Health: Current expenditurek,l (% of GDP)", 
+                        "Health: Physicians (per 1 000 pop.)",
+                        "Education: Government expenditure (% of GDP)",
+                        "Education: Primary gross enrol. ratio (f/m per 100 pop.)",
+                        "Education: Secondary gross enrol. ratio (f/m per 100 pop.)",
+                        "Education: Upper secondary gross enrol. ratio (f/m per 100 pop.)",
+                        "Intentional homicide rate (per 100 000 pop.)", 
+                        "Seats held by women in national parliaments (%)",
+                        "Individuals using the Internet (per 100 inhabitants)",
+                        "Research & Development expenditure (% of GDP)",
+                        "Threatened species (number)","Forested areah (% of land area)",
+                        "CO2 emission estimates (million tons/tons per capita)",
+                        "Energy production, primary (Petajoules)",
+                        "Energy supply per capita (Gigajoules)",
+                        "Tourist/visitor arrivals at national borderss (000)",
+                        "Important sites for terrestrial biodiversity protected (%)"),
+        Singapore = c("0.9","100","NULL","1.2","85.5/81.2","12.4/21.9","2,523.6/43.1",
+                      "1.3","1.6","4.5","2.3","NULL","100.3","106.3","111.7", "0.2", "29.5",
+                      "88.9","1.9","350","22.5","47.4/8.4","26","186","15,119","21.1"),
+        Malaysia = c("1.3","76.6","NULL","2","78.1/74.0", "23.3/11.3","3,476.6/10.7","188.1",
+                     "5.9","3.8","NULL","4.2","106.0/104.6","87.0/80.7","83.8/73.4","NULL","14.9",
+                     "84.2","1","1,928","58.5","228.0/7.2","3,952","122","26,101","28.5")
+      )
+      datatable(comparison_df_social)
+    }
     
+  })
+  
+  output$comparisontext <- renderText({
+    if(input$tabs == "comparison"){
+      "Singapore and Malaysia, neighboring countries in Southeast Asia, have similarities in
+      their past, culture, and location. Once part of Malaysia, Singapore became independent
+      in 1965 and grew into a prosperous nation. Both places have diverse populations, 
+      including Malays, Chinese, and Indians. They use languages like English and Chinese. 
+      Economically, they are important in the region, being centers for global business. 
+      Malaysia and Singapore have good tourism since people love to visit Kuala Lumpur and
+      Penang in Malaysia and Marina Bay and Sentosa Island in Singapore."
+    }
+  })
+  
+  output$reference <- renderText({
+    if(input$tabs == "references"){
+      "Code guide: chatgpt<br>
+      Map: https://gadm.org/download_country.html
+      "
+    }
+  })
+  
 }
 
-# Run the application 
 shinyApp(ui = ui, server = server)
+
